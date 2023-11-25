83677e686c4ba67c157bddf9b6ec5e07da27bb07
ae95caba <ae95caba@gmail.com>
2023-11-25 14:10:01 -0300

    add logic and style to OpenCloseSign

--- src/components/OpenCloseSign.js
+++ src/components/OpenCloseSign.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment-timezone";
 
 export default function OpenCloseSign() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const updateStatus = () => {
+      // Set the time zone to Argentina
+      const argentinaTime = moment().tz("America/Argentina/Buenos_Aires");
+
+      // Define your store's opening hours
+      const weekdaysOpeningHour = 20; // Monday to Friday opening hour
+      const weekendsOpeningHour = 13; // Saturday and Sunday opening hour
+      const closingHour = 24; // Closing hour for both weekdays and weekends
+
+      // Get the current day and hour
+      const currentDay = argentinaTime.day();
+      const currentHour = argentinaTime.hour();
+
+      // Determine if the store is open based on the current time and day
+      const isStoreOpen =
+        (currentDay >= 1 &&
+          currentDay <= 5 &&
+          currentHour >= weekdaysOpeningHour) ||
+        (currentDay >= 6 &&
+          currentDay <= 7 &&
+          currentHour >= weekendsOpeningHour);
+
+      setIsOpen(isStoreOpen);
+    };
+
+    // Update the status initially
+    updateStatus();
+
+    // Set up an interval to update the status every minute (adjust as needed)
+    const intervalId = setInterval(updateStatus, 60000); // 60000 milliseconds = 1 minute
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="open-close-sign">
-      <p>Abierto</p>
+      <p>{isOpen ? "Abierto" : "Cerrado"}</p>
     </div>
   );
 }
