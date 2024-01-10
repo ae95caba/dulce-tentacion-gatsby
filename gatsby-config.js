c3ebf6ea86776aab2bcf880bad4a719340825e5f
ae95caba <ae95caba@gmail.com>
2024-01-10 00:22:56 -0300

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,21 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-784HFDBC5Z", // Google Analytics / GA
+        ],
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Dulce Tentacion Marcos Paz`,
