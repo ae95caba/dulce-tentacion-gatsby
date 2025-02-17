706612f2ea059958d03ed926a310906ec78a3df1
ae95caba <ae95caba@gmail.com>
2025-02-16 21:43:22 -0300

    fix: :bug: attempt to break previus fix to see what has that fixed it

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -32,11 +32,6 @@ const SEO = ({ title, description, image, url }) => {
       <meta property="og:title" content={metaTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:url" content={metaUrl} />
-      <meta
-        property="og:image"
-        itemprop="image"
-        content="https://res.cloudinary.com/dto1ctatc/image/upload/v1739735861/dulce-tentacion/meta-image_wd4l7m.png"
-      />
 
       {/* WhatsApp uses Open Graph, so these should work */}
 
@@ -44,10 +39,6 @@ const SEO = ({ title, description, image, url }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={metaTitle} />
       <meta name="twitter:description" content={metaDescription} />
-      <meta
-        name="twitter:image"
-        content="https://res.cloudinary.com/dto1ctatc/image/upload/v1739735861/dulce-tentacion/meta-image_wd4l7m.png"
-      />
     </Helmet>
   );
 };
