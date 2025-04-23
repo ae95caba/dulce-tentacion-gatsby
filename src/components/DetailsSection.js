86c58f7f1b3f8a438fe0a40f1a004eaac19ff725
ae95caba <ae95caba@gmail.com>
2025-04-23 01:55:02 -0300

    refactor: :sparkles: streamline DetailsSection layout and update index.scss styles
    Refactored the DetailsSection component to enhance the presentation of product details, replacing list items with paragraph elements for a cleaner look. Adjusted the index.scss file to modify padding and gap values, improving overall layout consistency and visual appeal.

--- src/components/DetailsSection.js
+++ src/components/DetailsSection.js
@@ -24,53 +24,45 @@ const DetailsSection = ({
   return (
     <div className="details-section">
       <h4>Detalle:</h4>
+      <p>
+        🛒{product.name}: <span>${product.price}</span>
+      </p>
+
+      {/* List of chosen flavors */}
       <ul>
-        {createDescriptionItem(true, product.name, product.price)}
-
-        {/* List of chosen flavors */}
-        <ul>
-          {chosenFlavours.map((flavour) => (
-            <li key={flavour}>🍦{flavour}</li>
-          ))}
-        </ul>
-
-        {/* Aderezos section */}
-        {(sauces.chosenSauces?.length > 0 || rocklets.included) && (
-          <div className="aderezos-section">
-            <h5>Aderezos:</h5>
-
-            {/* Salsas subsection */}
-            {sauces.chosenSauces?.length > 0 && (
-              <div className="salsas-subsection">
-                <h6>Salsas:</h6>
-                <ul>
-                  {sauces.chosenSauces.map((sauce) => (
-                    <li key={sauce}>
-                      {sauce} <span>${sauces.price}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-
-            {/* Rocklets subsection */}
-            {rocklets.included && (
-              <div className="rocklets-subsection">
-                <p>
-                  <h6>Rocklets: </h6> <span>${rocklets.price}</span>
-                </p>
-              </div>
-            )}
-          </div>
-        )}
-
-        {/* Only show total if there are add-ons */}
-        {hasAddOns && (
-          <p>
-            {product.name} + aderezos: <span>${priceWithAddOns}</span>
-          </p>
-        )}
+        {chosenFlavours.map((flavour) => (
+          <li key={flavour}>🍦{flavour}</li>
+        ))}
       </ul>
+
+      {/* Aderezos section */}
+      {(sauces.chosenSauces?.length > 0 || rocklets.included) && (
+        <div className="aderezos-section">
+          <h5>Aderezos:</h5>
+
+          {/* Salsas subsection */}
+          {sauces.chosenSauces?.length > 0 && (
+            <p>
+              <strong>Salsa de {sauces.chosenSauces[0]} </strong>
+              <span>${sauces.price}</span>
+            </p>
+          )}
+
+          {/* Rocklets subsection */}
+          {rocklets.included && (
+            <p>
+              <strong>Rocklets: </strong> <span>${rocklets.price}</span>
+            </p>
+          )}
+        </div>
+      )}
+
+      {/* Only show total if there are add-ons */}
+      {hasAddOns && (
+        <p>
+          {product.name} + aderezos: <span>${priceWithAddOns}</span>
+        </p>
+      )}
     </div>
   );
 };
